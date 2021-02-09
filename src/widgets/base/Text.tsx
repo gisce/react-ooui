588fe32d41dc3ee197119c16d2a14cd5c62071b1
Marc Güell Segarra <marc@ondori.dev>
2021-02-09 08:53:15 +0100

    New Text component

---
+++ src/widgets/base/Text.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Input } from "antd";
+import FormItem from "../../common/FormItem";
+import { Text as TextOoui } from "ooui";
+const { TextArea } = Input;
+
+type Props = {
+  ooui: TextOoui;
+};
+
+const Text = (props: Props) => {
+  const { ooui } = props;
+
+  return (
+    <FormItem name={ooui.id} label={ooui.label}>
+      <TextArea rows={4} />
+    </FormItem>
+  );
+};
+
+export default Text;
