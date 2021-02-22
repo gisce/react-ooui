a538060d98090a7792769cd079da65ada81042d9
Marc Güell Segarra <marc@ondori.dev>
2021-02-22 16:15:04 +0100

    Add new storybook for testing NewLine forms

---
+++ src/stories/NewLine.stories.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { Form } from "..";
+import "antd/dist/antd.css";
+import "@/tailwind.generated.css";
+
+export default {
+  component: Form,
+  title: "Components/Widgets/Forms/NewLineExample",
+};
+
+const Template = (args: any) => <Form {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  initialValues: {
+    name: "John Appleseed",
+    login: "john",
+    context_lang: "ca_ES",
+    context_tz: "Europe/Madrid",
+    password: "MaskedPassword",
+    signature: "John Appleseed\n___\nReach me at +34973748312",
+  },
+  arch: `
+  <form string="Users">
+    <group colspan="4" col="4">
+      <field name="name"/>
+      <newline/>
+      <field name="login" readonly="1"/>
+    </group>
+  </form>`,
+  fields: {
+    login: {
+      required: true,
+      size: 64,
+      string: "Login",
+      type: "char",
+      views: {},
+    },
+    name: {
+      required: true,
+      select: true,
+      size: 64,
+      string: "Name",
+      type: "char",
+      views: {},
+    },
+  },
+};
