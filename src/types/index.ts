c98124c2c70b45b2ba31646187105583c7c37694
Marc Güell Segarra <marc@ondori.dev>
2021-02-23 12:27:06 +0100

    New WidgetProps type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,29 @@
+import { Field as FieldOoui } from "ooui";
+
 type Strings = {
   [key: string]: string;
-}
+};
 
 type SearchFields = {
   primary: string[];
   secondary: string[];
-}
+};
 
 type TreeView = {
   arch: string;
   fields: any;
-}
+};
 
 type Column = {
   title: string;
   dataIndex: string;
   key: string;
   type?: string;
-}
+};
 
-export type { Strings, SearchFields, TreeView, Column };
+type WidgetProps = {
+  ooui: FieldOoui;
+  layout?: "horizontal" | "vertical";
+};
+
+export type { Strings, SearchFields, TreeView, Column, WidgetProps };
