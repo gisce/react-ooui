7d6b06244af691b788a60734937ea818a7c578e2
Marc Güell Segarra <marc@ondori.dev>
2021-02-23 12:26:02 +0100

    Float storybook

---
+++ src/stories/Float.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Float } from "..";
+import { Float as FloatOoui } from "ooui";
+import "antd/dist/antd.css";
+
+export default {
+  title: "Components/Widgets/Base/Float",
+};
+
+export const Default = (): React.ReactElement => {
+  const ooui = new FloatOoui({
+    name: "field",
+    string: "Lorem ipsum",
+  });
+  return <Float ooui={ooui} />;
+};
