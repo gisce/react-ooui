d83183b4c3e520efbda56bd63e6e79c44a88e644
Marc Güell Segarra <marc@ondori.dev>
2021-04-29 18:16:07 +0200

    Create basic behaviour for One2many + storybook. To be improved and finished.

--- src/widgets/base/One2many.tsx
+++ src/widgets/base/One2many.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Button } from "antd";
-import Field from "@/common/Field";
 import { One2many as One2manyOoui } from "ooui";
 import { WidgetProps } from "@/types";
+import { Form } from "@/index";
+import { SimpleTree } from "@/index";
+import { Form as FormOoui, Tree as TreeOoui } from "ooui";
+
 import {
   FileAddOutlined,
   EditOutlined,
@@ -10,27 +13,55 @@ import {
   LeftOutlined,
   RightOutlined,
   AlignLeftOutlined,
-  MinusOutlined,
 } from "@ant-design/icons";
+
+const ids = [6];
 
 export const One2many = (props: WidgetProps) => {
   const { ooui } = props;
-  const { id, readOnly, required } = ooui as One2manyOoui;
+  const {
+    id,
+    readOnly,
+    required,
+    relation,
+    views,
+    mode,
+  } = ooui as One2manyOoui;
+
+  const [viewMode, setViewMode] = useState<string>(mode[0]);
+  const [itemIndex, setItemIndex] = useState<number>(0);
 
   const separator = () => {
     return <div className="inline-block w-3" />;
   };
 
   const index = () => {
-    return <span className="pl-1 pr-1">(1/1)</span>;
+    return (
+      <span className="pl-1 pr-1">
+        ({itemIndex + 1}/{ids.length})
+      </span>
+    );
+  };
+
+  const toggleViewMode = () => {
+    if (mode.length > 1) {
+      setViewMode(viewMode === "form" ? "tree" : "form");
+    }
+  };
+
+  const getTitle = () => {
+    const type = viewMode === "form" ? FormOoui : TreeOoui;
+    const ooui = new type(views[viewMode].fields);
+    ooui.parse(views[viewMode].arch);
+    return (ooui as any).string;
   };
 
   const topBar = () => {
     return (
-      <div className="flex">
+      <div className="flex mb-2">
         <div className="h-8 flex flex-grow bg-gray-700 text-gray-200">
           <div className="h-full flex flex-col justify-center items-center">
-            <span className="pl-2 font-bold">Contactes de l'empresa</span>
+            <span className="pl-2 font-bold">{getTitle()}</span>
           </div>
         </div>
         <div className="h-8 flex-none pl-2">
@@ -42,11 +73,45 @@ export const One2many = (props: WidgetProps) => {
           {index()}
           <Button icon={<RightOutlined />} />
           {separator()}
-          <Button icon={<AlignLeftOutlined />} />
+          <Button icon={<AlignLeftOutlined />} onClick={toggleViewMode} />
         </div>
       </div>
     );
   };
 
-  return topBar();
+  const content = () => {
+    if (viewMode === "form") {
+      return (
+        <Form
+          model={relation}
+          id={6}
+          onCancel={() => {
+            console.log();
+          }}
+          onSubmitSucceed={(value: any) => {
+            console.log();
+          }}
+        />
+      );
+    }
+
+    return (
+      <SimpleTree
+        model={relation}
+        ids={ids}
+        formView={views.form}
+        treeView={views.tree}
+        onRowClicked={() => {
+          console.log();
+        }}
+      />
+    );
+  };
+
+  return (
+    <>
+      {topBar()}
+      {content()}
+    </>
+  );
 };
