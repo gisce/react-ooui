eea5a80ae1045ed38c8c28b2a611cb1e96a94714
Marc Güell Segarra <marc@ondori.dev>
2021-05-11 10:40:17 +0200

    Create new hook for getting dynamic modal width depending on a predefined constant

---
+++ src/hooks/useModalWidthDimensions.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const MODAL_WIDTH_CONSTANT = 0.7;
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useModalWidthDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return { modalWidth: windowDimensions.width * MODAL_WIDTH_CONSTANT };
+}
