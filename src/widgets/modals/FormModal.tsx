69c0c7339124166bb5de277d8e8f2da734a87e60
Marc Güell Segarra <marc@ondori.dev>
2021-05-23 12:02:18 +0200

    Refactor FormModal to extend Form props

--- src/widgets/modals/FormModal.tsx
+++ src/widgets/modals/FormModal.tsx
@@ -1,32 +1,15 @@
 import React from "react";
 import { Modal } from "antd";
-import Form from "@/widgets/views/Form";
+import Form, { FormProps } from "@/widgets/views/Form";
 import useModalWidthDimensions from "@/hooks/useModalWidthDimensions";
 
-type FormModalProps = {
+type FormModalProps = FormProps & {
   visible: boolean;
-  model: string;
-  id?: number;
-  getDataFromAction?: boolean;
-  onCancel?: () => void;
-  onSubmitSucceed?: (updatedObject: any) => void;
   title?: string;
-  readOnly?: boolean;
-  mustClearAfterSave?: boolean;
 };
 
 export const FormModal = (props: FormModalProps) => {
-  const {
-    visible,
-    onCancel,
-    onSubmitSucceed,
-    id,
-    model,
-    title = id ? "Detail" : "New",
-    getDataFromAction,
-    readOnly = false,
-    mustClearAfterSave = false,
-  } = props;
+  const { visible, id, title = id ? "Detail" : "New", ...rest } = props;
 
   const { modalWidth } = useModalWidthDimensions();
 
@@ -39,16 +22,7 @@ export const FormModal = (props: FormModalProps) => {
       closable={false}
       footer={null}
     >
-      <Form
-        id={id}
-        model={model}
-        getDataFromAction={getDataFromAction}
-        onCancel={onCancel}
-        onSubmitSucceed={onSubmitSucceed}
-        showFooter
-        readOnly={readOnly}
-        mustClearAfterSave={mustClearAfterSave}
-      />
+      <Form id={id} {...rest} />
     </Modal>
   );
 };
