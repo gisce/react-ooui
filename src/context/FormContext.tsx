bcad6f6be7616813c589ec35c30975be8b3e9a6f
Marc Güell Segarra <marc@ondori.dev>
2021-06-02 09:01:56 +0200

    Add parent model in FormContext

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export type FormContextType = {
   parentId?: number;
+  parentModel: string;
 };
 
 export const FormContext = React.createContext<FormContextType | null>(null);
@@ -9,14 +10,17 @@ export const FormContext = React.createContext<FormContextType | null>(null);
 const FormProvider = ({
   children,
   parentId,
+  parentModel,
 }: {
   children: React.ReactNode;
   parentId?: number;
+  parentModel: string;
 }): any => {
   return (
     <FormContext.Provider
       value={{
         parentId,
+        parentModel,
       }}
     >
       {children}
