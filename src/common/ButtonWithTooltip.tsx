36e68611daaa84376f9be8ef621c5e7354c779ae
Marc Güell Segarra <marc@ondori.dev>
2021-06-09 09:16:32 +0200

    Create new component Button with tooltip

---
+++ src/common/ButtonWithTooltip.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, Tooltip, ButtonProps } from "antd";
+
+type Props = ButtonProps & {
+  tooltip: string;
+};
+
+function ButtonWithTooltip(props: Props) {
+  const { tooltip, ...restProps } = props;
+
+  return (
+    <Tooltip title={tooltip}>
+      <Button {...restProps} />
+    </Tooltip>
+  );
+}
+
+export default ButtonWithTooltip;
