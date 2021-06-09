49c3b2a7a9af893550cceeabd54acc0ebd51b83c
Marc Güell Segarra <marc@ondori.dev>
2021-06-09 10:21:36 +0200

    Build bundle

--- dist/widgets/base/Many2one.js
+++ dist/widgets/base/Many2one.js
@@ -175,7 +175,7 @@ var Many2oneInput = function (props) {
             }
         });
     }); };
-    return (react_1.default.createElement(antd_1.Row, { gutter: 8 },
+    return (react_1.default.createElement(antd_1.Row, { gutter: 8, wrap: false },
         react_1.default.createElement(antd_1.Col, { flex: "auto" },
             react_1.default.createElement(antd_1.Input, { type: "text", value: text, onChange: onValueStringChange, disabled: readOnly, className: requiredClass, onBlur: onElementLostFocus })),
         react_1.default.createElement(antd_1.Col, { flex: "32px" },
