b48638d08f5f15393de3b34d8fee97d686620de5
Marc Güell Segarra <marc@ondori.dev>
2021-11-04 08:40:35 +0100

    Build bundle

--- dist/widgets/base/ProgressBar.js
+++ dist/widgets/base/ProgressBar.js
@@ -1,4 +1,8 @@
 "use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -18,6 +22,7 @@ exports.ProgressBarInput = exports.ProgressBar = void 0;
 var react_1 = __importDefault(require("react"));
 var antd_1 = require("antd");
 var Field_1 = __importDefault(require("@/common/Field"));
+var styled_components_1 = __importDefault(require("styled-components"));
 var ProgressBar = function (props) {
     return (react_1.default.createElement(Field_1.default, __assign({}, props),
         react_1.default.createElement(exports.ProgressBarInput, null)));
@@ -25,7 +30,15 @@ var ProgressBar = function (props) {
 exports.ProgressBar = ProgressBar;
 var ProgressBarInput = function (_a) {
     var value = _a.value;
-    return react_1.default.createElement(antd_1.Progress, { percent: value, className: "pr-1" });
+    return (react_1.default.createElement(StyledProgress, { percent: value, format: function (percent) {
+            return (percent || 0).toLocaleString("en-US", {
+                minimumIntegerDigits: 1,
+                maximumFractionDigits: 4,
+                useGrouping: false,
+            }) + "%";
+        } }));
 };
 exports.ProgressBarInput = ProgressBarInput;
+var StyledProgress = styled_components_1.default(antd_1.Progress)(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  .ant-progress-outer {\n    margin-right: -72px;\n    padding-right: 72px;\n  }\n"], ["\n  .ant-progress-outer {\n    margin-right: -72px;\n    padding-right: 72px;\n  }\n"])));
+var templateObject_1;
 //# sourceMappingURL=ProgressBar.js.map
