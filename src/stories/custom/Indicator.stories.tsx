f08b92f320852bb116df6a1c85b69e1dff86c5d1
Eduard Carrerars <ecarreras@gisce.net>
2022-02-24 16:01:52 +0100

    Add icon and suffix for Indicator

---
+++ src/stories/custom/Indicator.stories.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+import {Indicator} from "../../widgets/custom/Indicator";
+import { Indicator as IndicatorOoui } from "@gisce/ooui";
+import "antd/dist/antd.css";
+import LocaleProvider from "../../context/LocaleContext";
+
+export default {
+  title: "Components/Widgets/Custom/Indicator",
+};
+
+export const Default = (): React.ReactElement => {
+  const ooui = new IndicatorOoui({
+    name: "button_field",
+    string: "Lorem ipsum",
+  });
+  return (
+    <LocaleProvider lang="en_US">
+      <Indicator ooui={ooui} />
+    </LocaleProvider>
+  );
+};
+
+
+export const WithIconAndSuffix = (): React.ReactElement => {
+  const ooui = new IndicatorOoui({
+    name: "test",
+    string: "Pending amount",
+    widget_props: "{'icon': 'wallet', 'suffix': '€'}",
+  });
+
+  return (
+    <LocaleProvider lang="en_US">
+      <Indicator ooui={ooui} value={300}/>
+    </LocaleProvider>
+  );
+};
+
+
+
+
