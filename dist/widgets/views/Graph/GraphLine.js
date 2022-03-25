6aef29face4d8362e04a9eee5f02a04cd6090d98
Marc Güell Segarra <marc@ondori.dev>
2022-03-25 09:19:19 +0100

    Build bundle

---
+++ dist/widgets/views/Graph/GraphLine.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GraphLine = void 0;
+var react_1 = __importDefault(require("react"));
+var icons_1 = require("@ant-design/icons");
+var plots_1 = require("@ant-design/plots");
+var useGraphCountData_1 = __importDefault(require("./useGraphCountData"));
+var antd_1 = require("antd");
+var GraphLine = function (props) {
+    var model = props.model, domain = props.domain, context = props.context, ooui = props.ooui;
+    var _a = useGraphCountData_1.default({
+        model: model,
+        domain: domain,
+        context: context,
+        x: ooui.x,
+        y: ooui.y,
+    }), data = _a.data, loading = _a.loading, error = _a.error, yLabel = _a.yLabel;
+    if (error) {
+        return react_1.default.createElement(antd_1.Alert, { message: JSON.stringify(error), type: "error", banner: true });
+    }
+    if (loading || data === undefined) {
+        return (react_1.default.createElement("div", { style: { padding: "1rem" } },
+            react_1.default.createElement(icons_1.LoadingOutlined, { style: { height: "12px" } })));
+    }
+    return (react_1.default.createElement("div", { style: { padding: "1rem" } },
+        react_1.default.createElement(plots_1.Line, { data: data, padding: "auto", xField: ooui.x.name, yField: yLabel, xAxis: { tickCount: 5 } })));
+};
+exports.GraphLine = GraphLine;
+//# sourceMappingURL=GraphLine.js.map
