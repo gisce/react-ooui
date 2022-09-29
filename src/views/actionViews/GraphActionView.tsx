456cc213a73ce68796d4824cd1f6e9f83c86940c
Marc Güell Segarra <marc@ondori.dev>
2022-09-29 11:18:45 +0200

    Allow Graph refreshing

--- src/views/actionViews/GraphActionView.tsx
+++ src/views/actionViews/GraphActionView.tsx
@@ -1,7 +1,12 @@
 import GraphActionBar from "@/actionbar/GraphActionBar";
 import TitleHeader from "@/ui/TitleHeader";
 import { Graph } from "@/widgets/views/Graph/Graph";
-import React from "react";
+import React, { useContext, useRef } from "react";
+import {
+  ActionViewContext,
+  ActionViewContextType,
+} from "@/context/ActionViewContext";
+import { FormView, TreeView } from "@/types";
 
 export type GraphActionViewProps = {
   viewData: any;
@@ -9,26 +14,44 @@ export type GraphActionViewProps = {
   model: string;
   context: any;
   domain: any;
+  formView: FormView;
+  treeView: TreeView;
 };
 
 export const GraphActionView = (props: GraphActionViewProps) => {
-  const { viewData, visible, model, context, domain } = props;
+  const { viewData, visible, model, context, domain, formView, treeView } =
+    props;
+
+  const { searchParams } = useContext(
+    ActionViewContext
+  ) as ActionViewContextType;
+
+  if (!visible) {
+    return null;
+  }
+
   if (!viewData) {
     return null;
   }
 
-  return (
-    <div style={!visible ? { display: "none" } : {}}>
-      <TitleHeader title={viewData.name}>
-        <GraphActionBar />
-      </TitleHeader>
+  const graphRef = useRef();
 
+  return (
+    <>
+      <TitleHeader title={viewData.name}>
+        <GraphActionBar
+          refreshGraph={() => {
+            (graphRef.current as any).refresh();
+          }}
+        />
+      </TitleHeader>
       <Graph
+        ref={graphRef}
         view_id={viewData.view_id}
         model={model}
         context={context}
-        domain={domain}
+        domain={searchParams || domain}
       />
-    </div>
+    </>
   );
 };
