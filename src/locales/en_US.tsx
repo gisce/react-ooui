13ba5b3de97b62b4c788d75a85275b96df92d22d
Marc Güell Segarra <marc@ondori.dev>
2022-12-01 12:47:27 +0100

    Add message popup when record is successfully saved

--- src/locales/en_US.tsx
+++ src/locales/en_US.tsx
@@ -81,4 +81,5 @@ export default {
   openAttachmentLink: "Open attachment link",
   uninformed: "Uninformed",
   totalRegisters: "Total registers:",
+  savedRegisters: "Record successfully saved",
 };
