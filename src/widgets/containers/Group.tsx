d94dafffbd3e2f0ffdfc7ab68615c64cc16e7717
Eduard Carrerars <ecarreras@gisce.net>
2023-05-05 15:28:08 +0200

    Add suport for group icon

--- src/widgets/containers/Group.tsx
+++ src/widgets/containers/Group.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Group as GroupOoui } from "@gisce/ooui";
 import Container from "./Container";
 import Fieldset from "@/ui/Fieldset";
+import { Space } from "antd";
+import iconMapper from "@/helpers/iconMapper";
 
 type Props = {
   ooui: GroupOoui;
@@ -11,11 +13,18 @@ type Props = {
 
 function Group(props: Props): React.ReactElement {
   const { ooui, showLabel = true, responsiveBehaviour } = props;
+  const Icon : React.ElementType | undefined = iconMapper(ooui.icon || "");
+  const label = (
+    <Space>
+        {Icon ? <Icon /> : null}
+        {ooui.label}
+      </Space>
+  )
 
   return (
     <>
       {ooui.label && showLabel ? (
-        <Fieldset label={ooui.label}>
+        <Fieldset label={label}>
           <Container
             container={ooui!.container}
             responsiveBehaviour={responsiveBehaviour}
