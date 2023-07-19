b4b7d8f7931d6586034d44d502ebbe03382107fd
Eduard Carrerars <ecarreras@gisce.net>
2023-07-19 11:31:12 +0200

    Better tags colors

---
+++ src/stories/Utils.stories.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import React from "react";
+import { CustomTag } from "../widgets/custom/Tag";
+import { colorFromString } from "../helpers/formHelper";
+
+export default {
+  title: "Components/Pure",
+};
+
+
+
+export const CustomTagWithColor = (): React.ReactElement => {
+  const text = "Hacker";
+  const color = colorFromString(text);
+  return <CustomTag color={color}>{text}</CustomTag>
+};
