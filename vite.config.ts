54af8eea8be16b1f008d33daa7b9d8f9be39fd85
Marc Güell Segarra <marc@ondori.dev>
2023-10-10 15:59:07 +0200

    fix: Improve bundle and add analyze script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     peerDepsExternal({
       includeDependencies: true,
-    }),
+    }) as PluginOption,
     react(),
     dts({
       insertTypesEntry: true,
@@ -26,14 +26,17 @@ export default defineConfig({
       fileName: (format) => `react-ooui.${format}.js`,
     },
     rollupOptions: {
-      external: ["@ant-design/icons", "uuid", "lodash", "moment"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "styled-components": "styled",
-        },
-      },
+      external: [
+        "@ant-design/icons",
+        "uuid",
+        "lodash/debounce",
+        "moment",
+        "dayjs",
+        "dayjs/plugin/relativeTime",
+        "dayjs/plugin/weekday",
+        "dayjs/plugin/localeData",
+        "dayjs/plugin/customParseFormat"
+      ], // We manually add these since they are not directly in package.json, but they end up in the bundle if we don't exclude them
     },
   },
 });
