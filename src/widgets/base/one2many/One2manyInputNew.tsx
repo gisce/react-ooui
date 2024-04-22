0e53330a13a69d735ba59faa12f8c0868d5ebc9f
Marc Güell Segarra <marc@ondori.dev>
2024-04-22 12:55:31 +0200

    feat(one2many-lazyload): refactor

--- src/widgets/base/one2many/One2manyInputNew.tsx
+++ src/widgets/base/one2many/One2manyInputNew.tsx
@@ -8,18 +8,23 @@ import { One2manyTree } from "./One2manyTree";
 import { One2many as One2manyOoui } from "@gisce/ooui";
 import { FormContext, FormContextType } from "@/context/FormContext";
 import { useOne2ManyDomain } from "./useOne2manyDomain";
-import { Button } from "antd";
 import { useOne2manyTree } from "./useOne2manyTree";
 import { One2manyTopBar } from "./One2manyTopBar";
 import { ViewType } from "@/types";
 import { useOne2manyForm } from "./useOne2manyForm";
+import { useOne2manyItems } from "./useOne2manyItems";
+import { One2manyForm } from "./One2manyForm";
+import { One2manyItem } from "./One2manyInput";
+import showUnsavedChangesDialog from "@/ui/UnsavedChangesDialog";
+import { useLocale } from "@gisce/react-formiga-components";
 
 export const One2manyInput: React.FC<One2manyInputProps> = (
   props: One2manyInputProps,
 ) => {
   const { value, onChange, ooui, views } = props;
-  const { items = [] } = value || {};
-  const { currentView, setCurrentView } = useContext(
+  const { items: one2manyItems = [] } = value || {};
+  const items = useOne2manyItems({ one2manyItems });
+  const { currentView, setCurrentView, itemIndex, setItemIndex } = useContext(
     One2manyContext,
   ) as One2manyContextType;
   const {
@@ -47,6 +52,8 @@ export const One2manyInput: React.FC<One2manyInputProps> = (
     context,
   });
 
+  const { t } = useLocale();
+
   const title = useMemo(() => {
     const { title } = views.get(currentView) || {};
     return title || ooui.label;
@@ -54,18 +61,87 @@ export const One2manyInput: React.FC<One2manyInputProps> = (
 
   const isMany2many = ooui.type === "many2many";
 
-  const { formHasChanges } = useOne2manyForm();
+  const {
+    formHasChanges,
+    setFormHasChanges,
+    getOriginalItemsWithRestoredItemId,
+  } = useOne2manyForm({
+    items,
+    formView: views.get("form"),
+    treeView: views.get("tree"),
+    relation,
+    context,
+  });
 
-  const toggleViewMode = useCallback(() => {
+  const toggleViewMode = () => {
     const keys = Array.from(views.keys());
     const nextView = keys[
       (keys.indexOf(currentView) + 1) % keys.length
     ] as ViewType;
-    setCurrentView(nextView);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentView, views]);
+    if (currentView === "form") {
+      showFormChangesDialogIfNeeded(() => {
+        setCurrentView(nextView);
+      });
+    } else {
+      setCurrentView(nextView);
+    }
+  };
+
   const showToggleButton = views.size > 1;
   const showCreateButton = views.get("form")?.fields !== undefined;
+
+  const triggerChange = useCallback(
+    (changedValues: One2manyItem[]) => {
+      onChange?.({
+        fields: views.get("form")?.fields || views.get("tree")?.fields,
+        items: changedValues,
+      });
+    },
+    [onChange, views],
+  );
+
+  const onFormChanges = useCallback(
+    (newUpdatedItems: One2manyItem[]) => {
+      setFormHasChanges(true);
+      triggerChange(newUpdatedItems);
+    },
+    [setFormHasChanges, triggerChange],
+  );
+
+  const onRowClicked = useCallback(
+    (item: any) => {
+      // find the index of the item in the items array
+      const index = items.findIndex((i) => i.id === item.id);
+      setItemIndex(index);
+      setCurrentView("form");
+    },
+    [items, setCurrentView, setItemIndex],
+  );
+
+  const reloadOriginalValuesForCurrentItem = useCallback(async () => {
+    const { id } = items[itemIndex];
+    if (!id) return;
+    const originalItems = await getOriginalItemsWithRestoredItemId({ id });
+    triggerChange(originalItems);
+  }, [getOriginalItemsWithRestoredItemId, itemIndex, items, triggerChange]);
+
+  const showFormChangesDialogIfNeeded = useCallback(
+    (callback: () => void) => {
+      if (formHasChanges) {
+        showUnsavedChangesDialog({
+          t,
+          onOk: () => {
+            reloadOriginalValuesForCurrentItem();
+            callback();
+            setFormHasChanges(false);
+          },
+        });
+      } else {
+        callback();
+      }
+    },
+    [formHasChanges, reloadOriginalValuesForCurrentItem, setFormHasChanges, t],
+  );
 
   return (
     <>
@@ -76,7 +152,7 @@ export const One2manyInput: React.FC<One2manyInputProps> = (
         isMany2Many={isMany2many}
         formHasChanges={formHasChanges}
         totalItems={items.length}
-        currentItemIndex={0}
+        currentItemIndex={itemIndex}
         onDelete={() => {}}
         onCreateItem={() => {}}
         onToggleViewMode={toggleViewMode}
@@ -94,25 +170,35 @@ export const One2manyInput: React.FC<One2manyInputProps> = (
           onFetchRecords={onTreeFetchRows}
           ooui={treeOoui}
           context={context}
-          onRowClicked={() => {}}
+          onRowClicked={onRowClicked}
         />
       )}
-      {currentView !== "tree" && (
-        <>
-          <span style={{ fontWeight: "bold" }}> {currentView}</span>
-          <Button
-            onClick={() => {
-              setCurrentView("tree");
-            }}
-          >
-            Change to tree
-          </Button>
-          <br />
-          {items.map((item) => {
-            return JSON.stringify(item);
-          })}
-        </>
+      {currentView === "form" && (
+        <One2manyForm
+          items={items}
+          formView={views.get("form")}
+          context={context}
+          relation={relation}
+          readOnly={readOnly || false}
+          onChange={onFormChanges}
+        />
       )}
+      {/* {currentView !== "tree" && ( */}
+      {/*   <> */}
+      {/*     <span style={{ fontWeight: "bold" }}> {currentView}</span> */}
+      {/*     <Button */}
+      {/*       onClick={() => { */}
+      {/*         setCurrentView("tree"); */}
+      {/*       }} */}
+      {/*     > */}
+      {/*       Change to tree */}
+      {/*     </Button> */}
+      {/*     <br /> */}
+      {/*     {items.map((item) => { */}
+      {/*       return JSON.stringify(item); */}
+      {/*     })} */}
+      {/*   </> */}
+      {/* )} */}
     </>
   );
 };
--- src/widgets/base/one2many/useOne2manyForm.ts
+++ src/widgets/base/one2many/useOne2manyForm.ts
@@ -1,10 +1,73 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { One2manyItem } from "./One2manyInput";
+import { TreeView, FormView } from "@/types";
+import ConnectionProvider from "@/ConnectionProvider";
+import {
+  One2manyContext,
+  One2manyContextType,
+} from "@/context/One2manyContext";
 
-export const useOne2manyForm = () => {
+export const useOne2manyForm = ({
+  items,
+  relation,
+  context,
+  treeView,
+  formView,
+}: {
+  items: One2manyItem[];
+  context: any[];
+  relation: string;
+  treeView: TreeView;
+  formView: FormView;
+}) => {
   const [formHasChanges, setFormHasChanges] = useState<boolean>(false);
+  const { itemIndex } = useContext(One2manyContext) as One2manyContextType;
+
+  // Whenever we save the form, we must reset the formHasChanges state
+  useEffect(() => {
+    const currentItem = items[itemIndex];
+    if (currentItem.operation === "original") {
+      setFormHasChanges(false);
+    }
+  }, [itemIndex, items]);
+
+  const getOriginalItemsWithRestoredItemId = async ({ id }: { id: number }) => {
+    const updatedFormObject = (
+      await ConnectionProvider.getHandler().readObjects({
+        model: relation,
+        ids: [id],
+        fields: formView.fields,
+        context,
+      })
+    )[0];
+
+    const updatedTreeObject = (
+      await ConnectionProvider.getHandler().readObjects({
+        model: relation,
+        ids: [id],
+        fields: treeView.fields,
+        context,
+      })
+    )[0];
+
+    const updatedItems: One2manyItem[] = items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          operation: "original",
+          values: updatedFormObject,
+          treeValues: updatedTreeObject,
+        };
+      }
+      return item;
+    });
+
+    return updatedItems;
+  };
 
   return {
     formHasChanges,
     setFormHasChanges,
+    getOriginalItemsWithRestoredItemId,
   };
 };
