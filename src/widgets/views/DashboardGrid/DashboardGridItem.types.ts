62dc429cfcfa61a392a63950c78ef86c49d5c7f4
Marc Güell Segarra <marc@ondori.dev>
2024-05-15 11:39:15 +0200

    fix: use board line id for keys in grid items

--- src/widgets/views/DashboardGrid/DashboardGridItem.types.ts
+++ src/widgets/views/DashboardGrid/DashboardGridItem.types.ts
@@ -2,7 +2,6 @@ import { ShortcutApi } from "@/ui/FavouriteButton";
 
 export type DashboardGridItemProps = {
   title: string;
-  id: string;
   children?: React.ReactNode;
   parms: any;
   action?: ShortcutApi;
