eeaec4ec7acbc1a93cb302723c2ea965c1eb67aa
Marc Güell Segarra <marc@ondori.dev>
2024-05-30 21:31:51 +0200

    fix: adjust memo in useOne2ManyItems

--- src/widgets/base/one2many/useOne2manyItems.ts
+++ src/widgets/base/one2many/useOne2manyItems.ts
@@ -1,9 +1,12 @@
 import { One2manyItem } from "./One2manyInput";
+import { useDeepCompareMemo } from "use-deep-compare";
 
 export const useOne2manyItems = ({
   one2manyItems,
 }: {
   one2manyItems: One2manyItem[];
 }) => {
-  return one2manyItems.filter((item) => item.operation !== "pendingRemove");
+  return useDeepCompareMemo(() => {
+    return one2manyItems.filter((item) => item.operation !== "pendingRemove");
+  }, [one2manyItems]);
 };
