07d76e153f206e0f51321abbedf9337538d405bb
Marc Güell Segarra <marc@ondori.dev>
2024-06-18 11:24:05 +0200

    feat: apply merge fields with all the fields of the form with the parsed context from arch
    https://github.com/gisce/webclient/issues/987

---
+++ src/helpers/fieldsHelper.ts
@@ -0,0 +1,13 @@
+export const mergeFieldsContext = (fields: any, contextFields: any) => {
+  const output: any = {};
+
+  Object.keys(fields).forEach((key) => {
+    output[key] = fields[key];
+    if (contextFields?.[key]) {
+      console.log(contextFields[key]);
+      output[key].context = contextFields[key];
+    }
+  });
+
+  return output;
+};
