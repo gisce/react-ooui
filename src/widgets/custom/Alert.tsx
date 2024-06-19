d1a3b5140dda458cded99cbdcb1014219f9c2ac6
Eduard Carrerars <ecarreras@gisce.net>
2024-06-19 11:07:56 +0200

    feat(widget): add new alert widget

---
+++ src/widgets/custom/Alert.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Alert as AntdAlert } from "antd";
+import { WidgetProps } from "@/types";
+import { Alert as AlertOoui } from "@gisce/ooui";
+import { Interweave } from "interweave";
+import iconMapper from "@/helpers/iconMapper";
+
+export const Alert = (props: WidgetProps) => {
+  const { ooui } = props;
+  const { title, text, alertType, icon } = ooui as AlertOoui;
+
+  function getIcon(icon: string): React.JSX.Element | undefined {
+    if (icon) {
+      const Icon: React.ElementType = iconMapper(icon) as any;
+      return Icon && <Icon />;
+    }
+    return undefined;
+  }
+
+  return (
+    <AntdAlert
+      message={<Interweave content={title} />}
+      description={<Interweave content={text} />}
+      type={alertType}
+      showIcon
+      icon={getIcon(icon)}
+    />
+  );
+};
