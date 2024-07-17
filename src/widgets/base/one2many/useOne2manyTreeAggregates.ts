22207b358e1182804838aa7ea42b3d7b3430ff21
Marc Güell Segarra <marc@ondori.dev>
2024-07-17 12:30:12 +0200

    feat: add aggregates to infinite tree in one2many

---
+++ src/widgets/base/one2many/useOne2manyTreeAggregates.ts
@@ -0,0 +1,28 @@
+import { Tree as TreeOoui } from "@gisce/ooui";
+import { One2manyItem } from "./One2manyInput";
+import { TreeAggregates, useTreeAggregates } from "./useTreeAggregates";
+
+export const useOne2manyTreeAggregates = ({
+  ooui,
+  items,
+  selectedRowKeys,
+  model,
+}: {
+  ooui: TreeOoui;
+  items: One2manyItem[];
+  selectedRowKeys: any[];
+  model: string;
+}): TreeAggregates => {
+  const realItems = items.filter((it) => it.id && it.id > 0);
+  let domain;
+  if (selectedRowKeys.length > 0) {
+    domain = [["id", "in", selectedRowKeys]];
+  } else if (realItems.length > 0) {
+    domain = [["id", "in", realItems.map((it) => it.id)]];
+  }
+  return useTreeAggregates({
+    ooui,
+    model,
+    domain,
+  });
+};
