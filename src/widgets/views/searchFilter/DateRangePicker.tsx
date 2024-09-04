7203d5cfabf2bb5fd2f1dc922fcaca55b32641c1
Marc Güell Segarra <marc@ondori.dev>
2024-09-04 15:45:24 +0200

    fix: use locale for datepickers

--- src/widgets/views/searchFilter/DateRangePicker.tsx
+++ src/widgets/views/searchFilter/DateRangePicker.tsx
@@ -1,15 +1,18 @@
 import { DatePicker } from "antd";
 
-import React from "react";
 import Field from "@/common/Field";
 import { WidgetProps } from "@/types";
+import { useDatePickerLocale } from "@/helpers/useDatePickerLocale";
 
 export const DateRangePicker = (props: WidgetProps) => {
+  const datePickerLocale = useDatePickerLocale();
+
   return (
     <Field {...props} layout={"vertical"}>
       <DatePicker.RangePicker
         allowEmpty={[true, true]}
         format={"DD/MM/YYYY"}
+        locale={datePickerLocale}
       ></DatePicker.RangePicker>
     </Field>
   );
