cbc4479a82733d1e02ac286e6aaf7219aed06b7e
Marc Güell Segarra <marc@ondori.dev>
2024-09-17 10:07:08 +0200

    feat: infinite one2many's improvements, sort and persist state (#590)
    * feat: implement user view prefs (remote columns for one2manytree)
    
    * fix: adjustment in useNetworkRequest and development mode
    
    * fix: remove console.log
    
    * fix: try to debug weird bug
    
    * fix: debug weird bug
    
    * Revert "fix: debug weird bug"
    
    This reverts commit 1235374b56987989b25a34d5152f37e48afdbd9d.
    
    * Revert "fix: try to debug weird bug"
    
    This reverts commit 51ae0e60d4f97c8ee8d82f09b29c59a04bdd1f31.
    
    * feat: allow sort columns in one2many infinite trees
    
    * fix: do not store sort column status in infinitetree's
    
    https://github.com/gisce/webclient/issues/978
    
    * feat: update gisce/react-formiga-table to v1.6.0 (#591)
    
    https://github.com/gisce/react-formiga-table/releases/tag/v1.6.0
    
    Co-authored-by: mguellsegarra <5711443+mguellsegarra@users.noreply.github.com>
    Co-authored-by: Marc Güell Segarra <marc@ondori.dev>
    
    * fix: update gisce/ooui to v2.10.2 (#592)
    
    https://github.com/gisce/ooui/releases/tag/v2.10.2
    
    Co-authored-by: mguellsegarra <5711443+mguellsegarra@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gisce <devel@gisce.net>
    Co-authored-by: mguellsegarra <5711443+mguellsegarra@users.noreply.github.com>

--- src/models/erpFeature.ts
+++ src/models/erpFeature.ts
@@ -3,6 +3,7 @@ export enum ErpFeatureKeys {
   FEATURE_ADVANCED_EXPORT = "export_data2",
   FEATURE_OMNISEARCH = "omnisearch",
   FEATURE_READFORVIEW = "read_for_view",
+  FEATURE_USERVIEWPREFS = "user_view_prefs",
   // ... add more features here
 }
 
