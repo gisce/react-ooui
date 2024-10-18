97b00e86aa3c13d9aed9d92e8347f5016be9a146
Eduard Carrerars <ecarreras@gisce.net>
2024-10-18 15:38:22 +0200

    feat(alert): allow action buttons inside alert widget

--- src/widgets/custom/Alert.tsx
+++ src/widgets/custom/Alert.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Alert as AntdAlert } from "antd";
+import { Alert as AntdAlert, Space } from "antd";
 import { WidgetProps } from "@/types";
 import { Alert as AlertOoui } from "@gisce/ooui";
 import { Interweave } from "interweave";
 import iconMapper from "@/helpers/iconMapper";
+import { Button } from "@/widgets/base/Button";
 
-export const Alert = (props: WidgetProps) => {
+type AlertOouiProps = WidgetProps & {
+  ooui: AlertOoui;
+};
+
+export const Alert = (props: AlertOouiProps) => {
   const { ooui } = props;
-  const { title, text, alertType, icon } = ooui as AlertOoui;
+  const { title, text, alertType, icon } = ooui;
 
   function getIcon(icon: string | null): React.JSX.Element | undefined {
     if (icon) {
@@ -17,12 +22,17 @@ export const Alert = (props: WidgetProps) => {
     return undefined;
   }
 
+  const buttons = ooui.buttons.map((button) => {
+    return <Button ooui={button} />;
+  });
+
   return (
     <AntdAlert
       message={<Interweave content={title} />}
       description={<Interweave content={text} />}
       type={alertType}
       showIcon
+      action={buttons ? <Space direction="vertical">{buttons}</Space> : null}
       icon={getIcon(icon)}
     />
   );
