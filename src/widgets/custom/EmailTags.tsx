91fe8178c450d5eba40f433328e5ad8d36279dd9
Gisce <devel@gisce.net>
2024-11-15 11:10:54 +0100

    feat: sync changes from v2 to v2-develop (#694) [skip ci]
    Co-authored-by: mguellsegarra <5711443+mguellsegarra@users.noreply.github.com>

---
+++ src/widgets/custom/EmailTags.tsx
@@ -0,0 +1,189 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useContext,
+} from "react";
+import { Input, Tag, theme } from "antd";
+import type { InputRef } from "antd";
+
+import Field from "@/common/Field";
+import { Email as EmailOOui } from "@gisce/ooui";
+import { WidgetProps } from "@/types";
+
+import validator from "validator";
+import { FormContext, FormContextType } from "@/context/FormContext";
+
+type EmailTagsProps = WidgetProps & {
+  ooui: EmailOOui;
+};
+
+export const EmailTags = (props: EmailTagsProps) => {
+  const { ooui } = props;
+  return (
+    <Field required={ooui.required} {...props}>
+      <EmailTagsInput maxLength={ooui.size} />
+    </Field>
+  );
+};
+
+interface EmailTagsRenderProps {
+  emails: string[] | string;
+  handleClose?: (email: string) => void;
+}
+
+export const EmailTagsRender: React.FC<EmailTagsRenderProps> = ({
+  emails,
+  handleClose,
+}) => {
+  if (!emails) {
+    return null;
+  }
+  const emailArray =
+    typeof emails === "string"
+      ? emails.split(";").map((email) => email.trim())
+      : emails;
+  return (
+    <>
+      {emailArray.map((email, index) => (
+        <Tag
+          key={index}
+          closable={!!handleClose}
+          color={validator.isEmail(email) ? undefined : "error"}
+          onClose={() => handleClose && handleClose(email)}
+        >
+          {email}
+        </Tag>
+      ))}
+    </>
+  );
+};
+
+interface EmailTagsInputProps {
+  maxLength?: number;
+  value?: string;
+  onChange?: (value: string) => void;
+  readonly?: boolean;
+}
+
+export const EmailTagsInput: React.FC<EmailTagsInputProps> = ({
+  value = "",
+  onChange,
+  maxLength,
+  readonly = false,
+}) => {
+  const { token } = theme.useToken();
+  const [emails, setEmails] = useState<string[]>(
+    value
+      ? value
+          .split(";")
+          .map((email) => email.trim())
+          .filter((email) => email)
+      : [],
+  );
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<InputRef>(null);
+  const formContext = useContext(FormContext) as FormContextType;
+  const { elementHasLostFocus } = formContext || {};
+
+  const handleChange = useCallback(
+    (newEmails: string[]) => {
+      if (onChange) {
+        onChange(newEmails.join(";"));
+      }
+    },
+    [onChange],
+  );
+
+  useEffect(() => {
+    handleChange(emails);
+  }, [handleChange, emails]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const parts = value.split(/[ ,;]+/);
+    const validEmails = parts
+      .slice(0, -1)
+      .map((email) => email.trim())
+      .filter((email) => email && !emails.includes(email));
+
+    const concatenatedEmails = [...emails, ...validEmails].join(";");
+    if (
+      !maxLength ||
+      concatenatedEmails.length + parts[parts.length - 1].length <= maxLength
+    ) {
+      if (validEmails.length > 0) {
+        setEmails([...emails, ...validEmails]);
+        handleChange([...emails, ...validEmails]);
+      }
+      setInputValue(parts[parts.length - 1]);
+    }
+  };
+
+  const handleInputConfirm = () => {
+    if (inputValue) {
+      const newEmails = inputValue
+        .split(";")
+        .map((email) => email.trim())
+        .filter((email) => email && !emails.includes(email));
+      const concatenatedEmails = [...emails, ...newEmails].join(";");
+      if (!maxLength || concatenatedEmails.length <= maxLength) {
+        setEmails([...emails, ...newEmails]);
+        handleChange([...emails, ...newEmails]);
+      }
+      setInputValue("");
+    }
+    if (elementHasLostFocus) {
+      elementHasLostFocus();
+    }
+  };
+
+  const handleClose = (removedEmail: string) => {
+    const filteredEmails = emails.filter((email) => email !== removedEmail);
+    setEmails(filteredEmails);
+    handleChange(filteredEmails);
+    if (elementHasLostFocus) {
+      elementHasLostFocus();
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        gap: token.sizeXS,
+        border: `${token.lineWidth}px ${token.lineType} ${token.colorBorder}`,
+        padding: token.paddingXS,
+        borderRadius: token.borderRadius,
+      }}
+      onClick={() => inputRef.current?.focus()}
+    >
+      <EmailTagsRender
+        emails={emails}
+        handleClose={!readonly ? handleClose : undefined}
+      />
+      <Input
+        readOnly={readonly}
+        ref={inputRef}
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onPressEnter={handleInputConfirm}
+        onBlur={handleInputConfirm}
+        style={{
+          flexGrow: 1,
+          minWidth: "100px",
+          border: "none",
+          outline: "none",
+          boxShadow: "none",
+          width: "auto",
+          marginLeft: 0,
+          paddingLeft: 0,
+        }}
+      />
+    </div>
+  );
+};
