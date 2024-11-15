6ee9f5cd8eb16d147845d85547c8ec1e53fec15c
mguellsegarra <5711443+mguellsegarra@users.noreply.github.com>
2024-11-15 10:28:57 +0000

    fix: sync changes from v2 to v2-develop

--- src/widgets/base/one2many/One2manyTree.tsx
+++ src/widgets/base/one2many/One2manyTree.tsx
@@ -5,7 +5,7 @@ import {
 } from "@gisce/react-formiga-table";
 import { One2manyItem } from "./One2manyInput";
 import { Tree as TreeOoui } from "@gisce/ooui";
-import { RefObject, useCallback, useRef } from "react";
+import { RefObject, useCallback, useMemo, useRef } from "react";
 import { getTableColumns, getTableItems } from "@/helpers/treeHelper";
 import { COLUMN_COMPONENTS } from "@/widgets/views/Tree/treeComponents";
 import useDeepCompareEffect from "use-deep-compare-effect";
@@ -22,16 +22,20 @@ import { useLocale } from "@gisce/react-formiga-components";
 
 export type One2manyTreeProps = {
   items: One2manyItem[];
-  onRowDoubleClick: (record: any) => void;
+  onRowDoubleClick?: (record: any) => void;
   readOnly: boolean;
   height?: number;
   ooui: TreeOoui;
   context: any;
   onFetchRecords: ({
-    idsToFetch,
+    allItems,
+    startRow,
+    endRow,
     sortFields,
   }: {
-    idsToFetch: number[];
+    allItems: One2manyItem[];
+    startRow: number;
+    endRow: number;
     sortFields?: Record<string, SortDirection>;
   }) => Promise<{
     results: any[];
@@ -47,6 +51,7 @@ export type One2manyTreeProps = {
   dataForHash: One2manyTreeDataForHash;
   aggregates?: TreeAggregates;
   selectedRowKeys?: number[];
+  showPointerCursorInRows?: boolean;
 };
 
 const DEFAULT_HEIGHT = 400;
@@ -68,6 +73,7 @@ export const One2manyTree = ({
   dataForHash,
   aggregates,
   selectedRowKeys = [],
+  showPointerCursorInRows = true,
 }: One2manyTreeProps) => {
   const internalGridRef = useRef<InfiniteTableRef>();
   const tableRef: RefObject<InfiniteTableRef> = gridRef! || internalGridRef!;
@@ -113,58 +119,11 @@ export const One2manyTree = ({
       endRow: number;
       sortFields?: Record<string, SortDirection>;
     }) => {
-      const itemsToFetch = itemsRef.current;
-      const idsToFetch = itemsToFetch.map((item) => item.id) as number[];
-      // now slice the records with startRow and endRow
-      const idsToFetchSliced = idsToFetch.slice(startRow, endRow);
-
-      // in this idsToFetchSliced we have the ids of the records that theoretically we have to fetch
-      // however, it's possible that these items have operation different than original,
-      // and we have to skip these items to being fetched, and passed later on to the callback as they were originally
-      const realIdsToFetch = idsToFetchSliced.filter((id) => {
-        const item = itemsToFetch.find((item) => item.id === id);
-        return (
-          item &&
-          (item.operation === "original" || item.operation === "pendingLink")
-        );
-      });
-      const otherItems = itemsToFetch.filter((item) => {
-        return (
-          item &&
-          item.operation !== "original" &&
-          item.operation !== "pendingLink"
-        );
-      });
-
-      if (realIdsToFetch.length === 0 && otherItems.length === 0) {
-        return [];
-      }
-
       const { results, colors, status } = await onFetchRecords({
-        idsToFetch: realIdsToFetch,
+        allItems: itemsRef.current,
+        startRow,
+        endRow,
         sortFields,
-      });
-
-      const preparedResults = getTableItems(ooui, results);
-
-      // now we have to map the results to the original ids
-      const resultsMapped = idsToFetchSliced.map((id) => {
-        const result = preparedResults.find((result) => result.id === id);
-        if (result) {
-          return result;
-        }
-        return itemsToFetch.find((item) => item.id === id)?.treeValues;
-      });
-
-      // Now we have to maintain the same order for resultsMapped that the one we have in preparedResults
-      resultsMapped.sort((a, b) => {
-        const indexA = preparedResults.findIndex(
-          (result) => result.id === a.id,
-        );
-        const indexB = preparedResults.findIndex(
-          (result) => result.id === b.id,
-        );
-        return indexA - indexB;
       });
 
       colorsForResults.current = { ...colorsForResults.current, ...colors };
@@ -174,9 +133,9 @@ export const One2manyTree = ({
       if (status) {
         statusForResults.current = { ...statusForResults.current, ...status };
       }
-      return resultsMapped;
+      return results;
     },
-    [onFetchRecords, ooui],
+    [onFetchRecords],
   );
 
   const onRowStyle = useCallback((record: any) => {
@@ -205,6 +164,7 @@ export const One2manyTree = ({
       columns={columns}
       onRequestData={onRequestData}
       onRowDoubleClick={onRowDoubleClick}
+      showPointerCursorInRows={showPointerCursorInRows}
       readonly={readOnly}
       onRowStyle={onRowStyle}
       onRowSelectionChange={onRowSelectionChange}
