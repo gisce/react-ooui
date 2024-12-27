e90b414f0ad0d7a2124dff26f024d2835230d9db
Gisce <devel@gisce.net>
2024-12-27 08:59:18 +0100

    feat: sync changes from v2 to v2-develop (#794) [skip ci]
    Co-authored-by: ecarreras <294235+ecarreras@users.noreply.github.com>

---
+++ src/widgets/custom/ColorPicker.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  ColorPicker as AntdColorPicker,
+  ColorPickerProps as AntColorPickerProps,
+  GetProp,
+} from "antd";
+import { ColorPicker as ColorPickerOoui } from "@gisce/ooui";
+
+import Field from "@/common/Field";
+import { WidgetProps } from "@/types";
+
+type ColorPickerProps = Omit<WidgetProps, "ooui"> & {
+  ooui: ColorPickerOoui;
+  onChange?: (value: string) => void;
+};
+
+export const ColorPicker = (props: ColorPickerProps) => {
+  const { ooui } = props;
+
+  return (
+    <Field {...props}>
+      <ColorPickerInput {...props} />
+    </Field>
+  );
+};
+
+export const ColorPickerInput = (
+  props: ColorPickerProps & { value?: any; onChange?: (value: any) => void },
+) => {
+  const { ooui, value, onChange } = props;
+
+  return (
+    <AntdColorPicker
+      showText={ooui.showText}
+      disabled={ooui.readOnly}
+      onChange={(value) => {
+        if (onChange) {
+          onChange(value.toHex());
+        }
+      }}
+      format="hex"
+      value={value}
+    />
+  );
+};
