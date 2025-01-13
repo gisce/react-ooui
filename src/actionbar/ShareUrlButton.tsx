5a2ef97ea3fd596f2ca582eee6049672070b2159
Marc Güell Segarra <marc@ondori.dev>
2025-01-13 17:54:24 +0100

    fix: control secure context
    https://github.com/gisce/webclient/issues/1612

--- src/actionbar/ShareUrlButton.tsx
+++ src/actionbar/ShareUrlButton.tsx
@@ -5,6 +5,7 @@ import { useLocale } from "@gisce/react-formiga-components";
 import { createShareOpenUrl } from "@/helpers/shareUrlHelper";
 import { ViewType } from "@/types";
 import { useCallback } from "react";
+import { ActionBarSeparator } from "./FormActionBar";
 
 export type ShareUrlButtonProps = {
   action_id?: number;
@@ -18,13 +19,17 @@ export function ShareUrlButton({
   res_id,
 }: ShareUrlButtonProps) {
   const { t } = useLocale();
-  let disabled = !action_id;
+
+  // Check if we're in a secure context
+  const isSecureContext = window.isSecureContext;
+
+  let moreDataNeededForCopying = !action_id;
   if (view_type === "form") {
-    disabled = !action_id || !res_id;
+    moreDataNeededForCopying = !action_id || !res_id;
   }
 
   const onClick = useCallback(() => {
-    if (disabled || !action_id) return;
+    if (!action_id) return;
     const url = createShareOpenUrl({
       action_id,
       view_type,
@@ -32,20 +37,13 @@ export function ShareUrlButton({
     });
     navigator.clipboard.writeText(url);
     message.success(t("urlCopiedToClipboard"));
-  }, [disabled, action_id, view_type, res_id, t]);
-
-  console.log({
-    disabled,
-    action_id,
-    view_type,
-    res_id,
-  });
+  }, [action_id, view_type, res_id, t]);
 
   return (
     <ActionButton
       icon={<ShareAltOutlined />}
+      disabled={!isSecureContext || moreDataNeededForCopying}
       tooltip={t("share")}
-      disabled={disabled}
       onClick={onClick}
     />
   );
