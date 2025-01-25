c79c3e0f124142cbee88a75dab8dbbcc3382fc96
Marc Güell Segarra <marc@ondori.dev>
2025-01-25 11:53:45 +0100

    fix: improve and export parameter filtering

--- src/index.ts
+++ src/index.ts
@@ -91,6 +91,7 @@ import { ErpAllFeatureKeys, ErpFeatureKeys } from "./models/erpFeature";
 import type { ErpFeaturesMap } from "./models/erpFeature";
 import { GraphCard } from "./widgets/views/Graph";
 import dayjs from "./helpers/dayjs";
+import { filterAllowedParameters , filterAllowedValues } from "./helpers/shareUrlHelper";
 
 export {
   Button,
@@ -183,4 +184,6 @@ export {
   Spinner,
   Carousel,
   ColorPicker,
+  filterAllowedParameters,
+  filterAllowedValues,
 };
