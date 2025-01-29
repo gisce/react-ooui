e89afa4e042cfd0f6105bad38998caaf9d569439
Marc Güell Segarra <marc@ondori.dev>
2025-01-29 10:24:01 +0100

    fix: ditch the view get API call in one2many for the moment (#864)
    https://github.com/gisce/webclient/issues/1745

--- src/widgets/base/one2many/One2many.tsx
+++ src/widgets/base/one2many/One2many.tsx
@@ -36,18 +36,13 @@ export const One2many = (props: Props) => {
   }, [ooui]);
 
   const getViewData = async (type: ViewType) => {
-    const getViewPromise = ConnectionProvider.getHandler().getView({
-      model: relation,
-      type,
-      context: { ...getContext?.(), ...context },
-    });
-
     if (oouiViews && oouiViews[type]) {
       const view = oouiViews[type];
-      if (!view.toolbar && (type === "form" || type === "tree")) {
-        const viewWithToolbar: TreeView | FormView = await getViewPromise;
-        return { ...view, toolbar: viewWithToolbar.toolbar };
-      }
+      // TODO: Replace with the new API call to retrieve only the toolbar
+      // if (!view.toolbar && (type === "form" || type === "tree")) {
+      //   const viewWithToolbar: TreeView | FormView = await getViewPromise;
+      //   return { ...view, toolbar: viewWithToolbar.toolbar };
+      // }
       return view;
     }
 
