4623b57c7cfdbc85c83b4fee440ef39aa71e477f
Eduard Carrerars <ecarreras@gisce.net>
2025-02-21 21:15:27 +0100

    feat(tree): allow sort by many2one if feature is enabled

--- src/helpers/treeHelper.tsx
+++ src/helpers/treeHelper.tsx
@@ -7,6 +7,8 @@ import {
 } from "@gisce/ooui";
 import { TreeView, Column } from "@/types";
 import { SortDirection, ColumnState } from "@gisce/react-formiga-table";
+import { useFeatureIsEnabled } from "@/context/ConfigContext";
+import { ErpFeatureKeys } from "@/models/erpFeature";
 
 const getTree = (treeView: TreeView): TreeOoui => {
   const xml = treeView.arch;
@@ -33,6 +35,9 @@ const getTableColumns = (
   context: any,
   treeType: "infinite" | "paginated" | "legacy",
 ): Column[] => {
+  const many2oneSortEnabled = useFeatureIsEnabled(
+    ErpFeatureKeys.FEATURE_MANY2ONE_SORT,
+  );
   const tableColumns = tree.columns.map((column) => {
     const type = column.type;
     const key = column.id;
@@ -66,7 +71,9 @@ const getTableColumns = (
       isSortable = type !== "one2many";
     } else {
       isSortable =
-        type !== "one2many" && type !== "many2one" && !column.isFunction;
+        type !== "one2many" &&
+        !column.isFunction &&
+        (type !== "many2one" || many2oneSortEnabled);
     }
 
     return {
--- src/models/erpFeature.ts
+++ src/models/erpFeature.ts
@@ -5,6 +5,7 @@ export enum ErpFeatureKeys {
   FEATURE_READFORVIEW = "read_for_view",
   FEATURE_USERVIEWPREFS = "user_view_prefs",
   FEATURE_GET_TOOLBAR = "get_toolbar",
+  FEATURE_MANY2ONE_SORT = "many2one_sort",
   // ... add more features here
 }
 
