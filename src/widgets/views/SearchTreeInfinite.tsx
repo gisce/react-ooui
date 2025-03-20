afce677bc206005de405ff0edb3560e872783b98
Marc Güell Segarra <marc@ondori.dev>
2025-03-20 14:05:16 +0100

    fix: avoid duplicate call

--- src/widgets/views/SearchTreeInfinite.tsx
+++ src/widgets/views/SearchTreeInfinite.tsx
@@ -173,7 +173,6 @@ function SearchTreeInfiniteComp(props: SearchTreeInfiniteProps, ref: any) {
       setSearchParams?.([]);
       setSearchValues?.({});
       tableRef.current?.unselectAll();
-      refresh();
     }
     prevNameSearch.current = nameSearch;
     // eslint-disable-next-line react-hooks/exhaustive-deps
