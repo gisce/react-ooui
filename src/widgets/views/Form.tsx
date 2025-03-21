c1f425b06bc4202a8291f3f92a484572d532578f
Marc Güell Segarra <marc@ondori.dev>
2025-03-21 08:25:12 +0100

    fix(Form): pass proper currentId value to form context (#987) [skip ci]
    * fix(Form): pass proper currentId value to form context
    
    https://github.com/gisce/webclient/issues/1958
    
    * fix: current id issues to form context
    
    https://github.com/gisce/webclient/issues/1958

--- src/widgets/views/Form.tsx
+++ src/widgets/views/Form.tsx
@@ -246,50 +246,11 @@ function Form(props: FormProps, ref: any) {
   const getCurrentId = useCallback(() => {
     return id || createdId.current;
   }, [id]);
-  const [refId, setRefId] = useState(() => createdId.current);
 
-  const onCancel = useCallback(() => {
-    if (mustFetchParentValues.current) {
-      onMustRefreshParent?.();
-    }
-    setFormIsSaving?.(false);
-    propsOnCancel?.({ id: getCurrentId(), values: getValues() });
-  }, [
-    getCurrentId,
-    getValues,
-    onMustRefreshParent,
-    propsOnCancel,
-    setFormIsSaving,
-  ]);
-
-  useEffect(() => {
-    if (createdId.current !== refId) {
-      setRefId(createdId.current);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [createdId.current]);
-
-  const currentId = useMemo(() => {
-    return id || refId;
-  }, [id, refId]);
+  const currentId = id || createdId.current;
 
   function getFields() {
     return fields;
-  }
-
-  function getValues() {
-    const values = {
-      ...getCurrentValues(fields),
-      ...getAdditionalValues(),
-    };
-
-    for (const key in values) {
-      if (values[key] === undefined) {
-        delete values[key];
-      }
-    }
-
-    return values;
   }
 
   function getPlainValues() {
@@ -391,6 +352,35 @@ function Form(props: FormProps, ref: any) {
     },
     [antForm],
   );
+
+  const getValues = useCallback(() => {
+    const values = {
+      ...getCurrentValues(fields),
+      ...getAdditionalValues(),
+    };
+
+    for (const key in values) {
+      if (values[key] === undefined) {
+        delete values[key];
+      }
+    }
+
+    return values;
+  }, [getCurrentValues, getAdditionalValues, fields]);
+
+  const onCancel = useCallback(() => {
+    if (mustFetchParentValues.current) {
+      onMustRefreshParent?.();
+    }
+    setFormIsSaving?.(false);
+    propsOnCancel?.({ id: getCurrentId(), values: getValues() });
+  }, [
+    getCurrentId,
+    getValues,
+    onMustRefreshParent,
+    propsOnCancel,
+    setFormIsSaving,
+  ]);
 
   const setFieldValue = (field: string, value?: string) => {
     assignNewValuesToForm({
@@ -1192,7 +1182,7 @@ function Form(props: FormProps, ref: any) {
           getPlainValues={getPlainValues}
           getFields={getFields}
           domain={actionDomain}
-          activeId={id}
+          activeId={currentId}
           activeModel={model}
           setFieldValue={setFieldValue}
           getFieldValue={getFieldValue}
