947ec3448cd3922041e7276daef434477bb80bd1
Marc Güell Segarra <marc@ondori.dev>
2025-04-30 13:32:46 +0200

    fix: infinite loop bug in attachments
    https://github.com/gisce/webclient/issues/2083

--- src/widgets/modals/TranslationModal.tsx
+++ src/widgets/modals/TranslationModal.tsx
@@ -48,6 +48,7 @@ export const TranslationModal = (props: TranslationModalProps) => {
     } else {
       setValuesForLangs({});
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [visible]);
 
   async function fetchData() {
