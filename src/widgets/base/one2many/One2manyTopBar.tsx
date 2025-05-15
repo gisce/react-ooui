f4ce6d3f39d56b783b631698a56ba2a922a9600a
Marc Güell Segarra <marc@ondori.dev>
2025-05-15 09:10:20 +0200

    feat: adjustments in o2m/m2m selection behaviour
    https://github.com/gisce/webclient/issues/2143

--- src/widgets/base/one2many/One2manyTopBar.tsx
+++ src/widgets/base/one2many/One2manyTopBar.tsx
@@ -92,6 +92,7 @@ function One2manyTopBarComponent(props: One2manyTopBarProps) {
       mustDisableButtons: readOnly,
       formRef,
       onRefreshParentValues,
+      totalItems,
     });
 
   const {
@@ -135,7 +136,7 @@ function One2manyTopBarComponent(props: One2manyTopBarProps) {
             totalItems={totalItems}
             readOnly={readOnly}
             mode={mode}
-            selectedRowKeys={selectedRowKeys}
+            selectedRowKeys={readOnly ? [] : selectedRowKeys}
             onDelete={onDelete}
           />
         )}
@@ -304,6 +305,7 @@ const ItemBrowser = memo(
           tooltip={t("previousItem")}
           icon={<LeftOutlined />}
           onClick={onPreviousItem}
+          disabled={totalItems === 0}
         />
         <ItemIndex
           currentItemIndex={currentItemIndex}
@@ -313,6 +315,7 @@ const ItemBrowser = memo(
           tooltip={t("nextItem")}
           icon={<RightOutlined />}
           onClick={onNextItem}
+          disabled={totalItems === 0}
         />
       </>
     );
