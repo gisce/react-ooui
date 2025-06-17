1011cc84ffee1906aa13899b674ac3b7bd541918
Marc Güell Segarra <marc@ondori.dev>
2025-06-17 14:04:05 +0200

    fix: migrate to storybook 9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,12 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 import path from "path";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
   async viteFinal(config) {
     // Set up aliases
