518613530151f1b62d8553460c9759104f53f5c7
Marc Güell Segarra <marc@ondori.dev>
2025-06-18 16:32:31 +0200

    fix: add new user feature for toolbar fetching

--- src/models/userFeature.ts
+++ src/models/userFeature.ts
@@ -1,6 +1,7 @@
 export enum UserFeatureKeys {
   FEATURE_MANY2ONE_DISABLE_ARROW_MENU = "widget.many2one.disable.arrow_menu",
   FEATURE_MANY2ONE_DISABLE_FOLDER = "widget.many2one.disable.folder",
+  FEATURE_GET_TOOLBAR_SORT_BY_LAST_RUN = "orm.get_toolbar.sort_by_last_run",
 }
 
 export const UserAllFeatureKeys: string[] = Object.values(UserFeatureKeys);
