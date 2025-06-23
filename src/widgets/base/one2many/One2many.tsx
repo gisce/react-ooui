b0e52ec698bdae7910eb77414110deab2230ea9f
Marc Güell Segarra <marc@ondori.dev>
2025-06-23 12:06:24 +0200

    fix: toolbar retrieval for one2many
    https://github.com/gisce/webclient/issues/2305#event-18269744207

--- src/widgets/base/one2many/One2many.tsx
+++ src/widgets/base/one2many/One2many.tsx
@@ -62,6 +62,24 @@ export const One2many = (props: Props) => {
       return view;
     }
 
+    if (getToolbarEnabled && (type === "form" || type === "tree")) {
+      // Get view and toolbar in parallel
+      const [viewData, toolbar] = await Promise.all([
+        ConnectionProvider.getHandler().getView({
+          model: relation,
+          type,
+          context: { ...getContext?.(), ...context },
+        }),
+        ConnectionProvider.getHandler().getToolbar({
+          model: relation,
+          type,
+          context: { ...getContext?.(), ...context },
+        }),
+      ]);
+      return { ...viewData, toolbar };
+    }
+
+    // If toolbar not enabled or not form/tree view, just get view
     return await ConnectionProvider.getHandler().getView({
       model: relation,
       type,
@@ -75,14 +93,17 @@ export const One2many = (props: Props) => {
 
     try {
       if (mode && mode.length > 0) {
-        for (const m of mode as ViewType[]) {
+        const viewPromises = (mode as ViewType[]).map(async (m: ViewType) => {
           const v = await getViewData(m);
           views.set(m, v);
-        }
+        });
+        await Promise.all(viewPromises);
         setViews(views);
       } else {
-        const formView = await getViewData("form");
-        const treeView = await getViewData("tree");
+        const [formView, treeView] = await Promise.all([
+          getViewData("form"),
+          getViewData("tree"),
+        ]);
         views.set("form", formView);
         views.set("tree", treeView);
         setViews(views);
