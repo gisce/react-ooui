d9f85926fdd3d4ee10a468118183a688be398161
Marc Güell Segarra <marc@ondori.dev>
2025-07-29 13:29:56 +0200

    fix: dependencies issue
    https://github.com/gisce/webclient/issues/2420

--- src/widgets/views/Tree/Tree.tsx
+++ src/widgets/views/Tree/Tree.tsx
@@ -146,7 +146,7 @@ export const UnmemoizedTree = forwardRef<TableRef, Props>(
       const items = getOldTableItems(treeOoui, results);
       setItems(items);
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [results]);
+    }, [results, treeOoui, context]);
 
     useEffect(() => {
       internalLimit.current = limit;
