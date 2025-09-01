dd21e85de5ded569bb4696da6598fc1379478898
Marc Güell Segarra <marc@ondori.dev>
2025-09-01 13:36:14 +0200

    feat: hide headers

--- src/widgets/views/Tree/Paginated/SearchTreePaginated.tsx
+++ src/widgets/views/Tree/Paginated/SearchTreePaginated.tsx
@@ -55,6 +55,7 @@ function SearchTreePaginatedComp(props: SearchTreePaginatedProps, ref: any) {
     nameSearch: nameSearchProps,
     filterType = "side",
     onChangeTreeType,
+    hideHeaders = false,
   } = props;
 
   // Refs
@@ -262,7 +263,7 @@ function SearchTreePaginatedComp(props: SearchTreePaginatedProps, ref: any) {
   // Render
   return (
     <Fragment>
-      {!isExpandable && (
+      {!isExpandable && !hideHeaders && (
         <PaginatedSearchControls
           filterType={filterType}
           formView={formView}
@@ -275,7 +276,7 @@ function SearchTreePaginatedComp(props: SearchTreePaginatedProps, ref: any) {
           onSideSearchFilterSubmit={onSideSearchFilterSubmit}
         />
       )}
-      {!isExpandable && (
+      {!isExpandable && !hideHeaders && (
         <PaginationHeader
           total={totalRowsAdjusted}
           totalRowsLoading={totalRowsLoading}
