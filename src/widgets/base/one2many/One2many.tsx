1992cb930b7c053b1a4e546245a1a3279a2f4112
Marc Güell Segarra <marc@ondori.dev>
2025-10-09 08:30:22 +0200

    Revert "Merge branch 'fix/one2many-always-get-tree-and-form' into alpha"
    This reverts commit fa21385a1279d578d4625e6d9ed8c604dcb4497f, reversing
    changes made to 8039a9b5d72f3d538b5551b9e6d3dbdd6e64bba6.

--- src/widgets/base/one2many/One2many.tsx
+++ src/widgets/base/one2many/One2many.tsx
@@ -117,11 +117,7 @@ export const One2many = (props: Props) => {
 
     try {
       if (mode && mode.length > 0) {
-        // Always ensure tree and form are included
-        const requiredViews: ViewType[] = ["tree", "form"];
-        const allViews = new Set([...requiredViews, ...(mode as ViewType[])]);
-
-        const viewPromises = Array.from(allViews).map(async (m: ViewType) => {
+        const viewPromises = (mode as ViewType[]).map(async (m: ViewType) => {
           const v = await getViewData(m);
           views.set(m, v);
         });
