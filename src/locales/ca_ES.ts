88675a04a712d7341166743935b7027bfdb6d828
Marc Güell Segarra <marc@ondori.dev>
2025-12-22 11:11:49 +0100

    feat(one2many): add button to open items in list view
    https://github.com/gisce/webclient/issues/272

--- src/locales/ca_ES.ts
+++ src/locales/ca_ES.ts
@@ -135,4 +135,5 @@ export default {
   favouriteName: "Nom del preferit",
   saveFavourite: "Desar preferit",
   enterFavouriteName: "Introdueix el nom del preferit",
+  openInListView: "Obrir en vista de llista",
 };
--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -132,4 +132,5 @@ export default {
   favouriteName: "Favourite name",
   saveFavourite: "Save favourite",
   enterFavouriteName: "Enter favourite name",
+  openInListView: "Open in list view",
 };
