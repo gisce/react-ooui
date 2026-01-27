3c96745a370d979cfdc0013b073ef7ccb15bf93e
Marc Güell Segarra <marc@ondori.dev>
2026-01-27 08:46:36 +0100

    fix: ditch domain in m2o lazy search fields (#1487) [skip ci]
    * fix: ditch domain in m2o lazy search fields
    
    https://github.com/gisce/webclient/issues/2907
    
    * fix: filter out dynamic domains in M2O lazy search fields
    
    https://github.com/gisce/webclient/issues/2907

--- src/widgets/views/searchFilter/SearchField.tsx
+++ src/widgets/views/searchFilter/SearchField.tsx
@@ -17,6 +17,7 @@ import { Many2oneLazyInput } from "@/widgets/base/many2one/Many2oneLazy";
 import FieldWrapper from "@/common/Field";
 import { useUserFeatureIsEnabled } from "@/context/ConfigContext";
 import { UserFeatureKeys } from "@/models/userFeature";
+import { getSafeSearchDomain } from "@/helpers/domainHelper";
 
 type Props = {
   field: Field;
@@ -52,7 +53,7 @@ export function SearchField(props: Props) {
       string: field.label,
       relation: fieldRelation,
       context: field.context,
-      domain: field.domain,
+      domain: getSafeSearchDomain((field as any).domain),
     });
     m2oOoui.parsedWidgetProps = {
       showCreate: false,
